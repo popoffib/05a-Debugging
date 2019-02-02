0c4c07290b8cb33e7820faa0ceb479c9c6a0fae1
popoffib <popoffib@rose-hulman.edu>
2019-02-02 16:42:04 -0500

    done

--- src/m1.py
+++ src/m1.py
@@ -315,7 +315,7 @@ def broken_6(n):
 
 
 # -----------------------------------------------------------------------------
-# TODO: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # -----------------------------------------------------------------------------
 def broken_7(n, point, length, distance_between_lines, window):
@@ -350,7 +350,7 @@ def broken_7(n, point, length, distance_between_lines, window):
         window.render(0.5)
         left = rg.Point(left.x, left.y + distance_between_lines)
         right = rg.Point(right.x, right.y + distance_between_lines)
-    window.close_on_mouse_click()
+    # window.close_on_mouse_click()
 
 
 # -----------------------------------------------------------------------------
